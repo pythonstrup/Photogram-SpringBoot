48e9ec4d89c3c67d45197106f128bb5ef36e92b6
BigBell <pythonstrup@gmail.com>
2022-03-04 15:02:50 +0900

    1.회원가입 기능

--- src/main/java/com/cos/photogramstart/ViewControllerTest.java
+++ src/main/java/com/cos/photogramstart/ViewControllerTest.java
@@ -1,9 +1,9 @@
-package com.cos.photogram;
+package com.cos.photogramstart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class ViewControllerTest {
 
 	@GetMapping("/auth/signup")
---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,60 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장할 수 있는 API를 제공)
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // DB에 테이블을 생성
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+	private int id;
+	
+	@Column(length=20, unique=true)
+	private String username;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	@Column(nullable=false)
+	private String name;
+	
+	private String website;
+	
+	private String bio;  // 자기소개
+	
+	@Column(nullable=false)
+	private String email;
+	
+	private String phone;
+	
+	private String gender;
+	
+	private String profileImageUrl;
+	
+	private String role; // 권한 
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // DB에 Insert가 진행되기 직전에 실행하는 어노테이션
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+}
